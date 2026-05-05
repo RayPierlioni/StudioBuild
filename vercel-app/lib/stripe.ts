ea622b0558d8801f1fc7c6ef548793fc61a16c04
RayPierlioni <57530356+RayPierlioni@users.noreply.github.com>
2026-05-05 05:30:17 -0400

    Add files via upload

--- vercel-app/lib/stripe.ts
+++ vercel-app/lib/stripe.ts
@@ -7,7 +7,7 @@ let cachedStripe: Stripe | null = null;
 export function getStripeClient() {
   if (!cachedStripe) {
     cachedStripe = new Stripe(requiredServerEnv("STRIPE_SECRET_KEY"), {
-      apiVersion: "2026-02-25.clover" as Stripe.LatestApiVersion,
+      apiVersion: "2026-02-25.clover",
     });
   }
 
